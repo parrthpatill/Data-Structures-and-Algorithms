09a94378117dfef84f8156d5fbe751fbc7f567a8
2023bit602 <parthpatil0512@gmail.com>
2025-04-17 02:24:14 +0530

    check if array is sorted and rotated

--- ArraysEasy/ArraysEasy.java
+++ ArraysEasy/ArraysEasy.java
@@ -52,10 +52,24 @@ class ArraysEasy{
 		return true;
 	}
 
+// check if the array is sorted and rotated
+	boolean checkSortedAndRotated(int[] arr){
+		int count = 0;
+		for (int i = 0; i < arr.length; i++){
+			if(arr[i] >arr[(i+1)%arr.length]){
+				count++;
+			}
+			if(count > 1){
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public static void main(String[] args){
 
 		ArraysEasy obj = new ArraysEasy();
-		int[] nums = {1,2,3,4,5};
-		System.out.println(obj.checkSorted(nums));
+		int[] nums = {3,4,5,1,2};
+		System.out.println(obj.checkSortedAndRotated(nums));
 	}
 }
