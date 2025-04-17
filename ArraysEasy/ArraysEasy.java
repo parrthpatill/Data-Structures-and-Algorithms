a141fd2719144bce0f4ab2726253844db5236e91
2023bit602 <parthpatil0512@gmail.com>
2025-04-18 02:09:29 +0530

    remove dupilcates from a sorted array

--- ArraysEasy/ArraysEasy.java
+++ ArraysEasy/ArraysEasy.java
@@ -66,10 +66,22 @@ class ArraysEasy{
 		return true;
 	}
 
+// remove duplicate from a sorted array and return the length
+	int removedDuplicates(int[] arr){
+		int i = 0;
+		for (int j = 0; j < arr.length; j++){
+			if(arr[j] != arr[i]){
+				arr[i+1] = arr[j];
+				i++;
+			}
+		}
+		return i+1;
+	}
+
 	public static void main(String[] args){
 
 		ArraysEasy obj = new ArraysEasy();
-		int[] nums = {3,4,5,1,2};
-		System.out.println(obj.checkSortedAndRotated(nums));
+		int[] nums = {1,1,1,2,2,3,4,4};
+		System.out.println(obj.removedDuplicates(nums));
 	}
 }
