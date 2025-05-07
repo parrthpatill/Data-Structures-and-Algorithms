a5b6661011216fdf740089b3a5b1ab30924df15a
2023bit602 <parthpatil0512@gmail.com>
2025-05-07 14:35:20 +0530

    ROTATE IMAGE brute and optimal

--- ArraysEasyMediumHard/ArraysMedium.java
+++ ArraysEasyMediumHard/ArraysMedium.java
@@ -511,17 +511,52 @@ class ArraysMedium{
 		}
 	}
 
+// ROTATE IMAGE/ ROTATE MATRIX BY 90deg(n x n)
+	int[][] rotateImageBrute(int[][] arr){
+		int n = arr.length;
+		int[][] ans = new int[n][n];
+		for(int i = 0; i<n; i++){
+			for(int j = 0; j<n; j++){
+				ans[j][(n-1)-i] = arr[i][j];
+			}
+		}
+		return ans;
+	}
+	void rotateImageOptimal(int[][] arr){
+		int n = arr.length;
+		for(int i = 0; i<n-1; i++){
+			for(int j = i+1; j<n; j++){
+				int temp = arr[i][j];
+				arr[i][j] = arr[j][i];
+				arr[j][i] = temp;
+			}
+		}
+		for(int i = 0; i<n; i++){
+			int left = 0, right = n-1;
+			while(left <= right){
+				int temp = arr[i][left];
+				arr[i][left] = arr[i][right];
+				arr[i][right] = temp;
+				left++;
+				right--;
+			}
+		}
+	}
+
 	public static void main(String[] args){
 		ArraysMedium obj = new ArraysMedium();
 		int[] arr1 = {102,4,100,1,101,3,2,1,1};
-		int[][] arr2 = { {1,1,1,1},
-						 {1,0,1,1},
-						 {1,1,0,1},
-						 {0,1,1,1} };
-		obj.setMatrixZerosOptimal(arr2);
+		int[][] arr2 = { {1,2,3,4},
+						 {5,6,7,8},
+						 {9,10,11,12},
+						 {13,14,15,16} };
+		obj.rotateImageOptimal(arr2);
+		// int[][] ans = obj.rotateImageBrute(arr2);
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
 				System.out.print(arr2[i][j]);
+				System.out.print(",");
+
 			}
 			System.out.println();
 		}
