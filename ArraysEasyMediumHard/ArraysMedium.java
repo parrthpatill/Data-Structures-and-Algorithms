82f34b86b509a6ea9c17eefdb791f55a261e771e
2023bit602 <parthpatil0512@gmail.com>
2025-05-07 11:27:36 +0530

    SET MATRIX ZEROS Optimal

--- ArraysEasyMediumHard/ArraysMedium.java
+++ ArraysEasyMediumHard/ArraysMedium.java
@@ -474,14 +474,51 @@ class ArraysMedium{
 		}
 	}
 
+	void setMatrixZerosOptimal(int[][] arr){
+		// for marking row = arr[i][0]
+		// for marking col = arr[0][j]
+		int n = arr.length;
+		int m = arr[0].length;
+		int col0 = 1;
+		for(int i=0; i<n; i++){
+			for(int j=0; j<m; j++){
+				if(arr[i][j] == 0){
+					arr[i][0] = 0;
+					if(j!=0){
+						arr[0][j] = 0;
+					} else{
+						col0 = 0;
+					}
+				}
+			}
+		}
+		for(int i = 1; i<n; i++){
+			for(int j = 1; j<m; j++){
+				if(arr[i][0] == 0 || arr[0][j] == 0){
+					arr[i][j] = 0;
+				}
+			}
+		}
+		if(arr[0][0] == 0){
+			for(int j = 0; j<m; j++){
+				arr[0][j] = 0;
+			}
+		}
+		if(col0 == 0){
+			for(int i = 0; i<n; i++){
+				arr[i][0] = 0;
+			}
+		}
+	}
+
 	public static void main(String[] args){
 		ArraysMedium obj = new ArraysMedium();
 		int[] arr1 = {102,4,100,1,101,3,2,1,1};
 		int[][] arr2 = { {1,1,1,1},
-						 {1,0,0,1},
+						 {1,0,1,1},
 						 {1,1,0,1},
-						 {1,1,1,1} };
-		obj.setMatrixZerosBrute(arr2);
+						 {0,1,1,1} };
+		obj.setMatrixZerosOptimal(arr2);
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
 				System.out.print(arr2[i][j]);
