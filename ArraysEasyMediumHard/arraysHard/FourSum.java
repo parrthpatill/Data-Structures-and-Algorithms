aad36709801782572ba78666e53c6c2aa005949e
2023bit602 <parthpatil0512@gmail.com>
2025-05-16 04:08:03 +0530

    4SUM better solution

--- ArraysEasyMediumHard/arraysHard/FourSum.java
+++ ArraysEasyMediumHard/arraysHard/FourSum.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class FourSum{
 	
+	// Brute force solution
 	List<List<Integer>> getFourSumBrute(int[] arr, int target){
 		int n = arr.length;
 		Set<List<Integer>> set = new HashSet<>();
@@ -11,7 +12,7 @@ public class FourSum{
 					for(int l = k+1; l<n; l++){
 						// initialising sum like this so as to now pass the integer limit 
 						// which will cause the type casting to fail
-						long sum = arr[i]+arr[j];
+						long sum = (long)arr[i]+arr[j];
 						sum += arr[k];
 						sum += arr[l];
 						if(sum == target){
@@ -21,6 +22,28 @@ public class FourSum{
 						}
 					}
 				}
+			}
+		}
+		List<List<Integer>> ans = new ArrayList<>(set);
+		return ans;
+	}
+
+	//Better Solution
+	List<List<Integer>> getFourSumBetter(int[] arr, int target){
+		int n = arr.length;
+		Set<List<Integer>> set = new HashSet<>();
+		for(int i=0; i<n; i++){
+			for(int j=i+1; j<n; j++){
+				Set<Integer> HashSet = new HashSet<>();
+				for(int k = j+1; k<n; k++){
+					int fourth = target - (arr[i]+arr[j]+arr[k]);
+					if(HashSet.contains(fourth)){
+						List<Integer> quad = Arrays.asList(arr[i],arr[j],arr[k],fourth);
+						Collections.sort(quad);
+						set.add(quad);
+					}
+					HashSet.add(arr[k]);
+				}	
 			}
 		}
 		List<List<Integer>> ans = new ArrayList<>(set);
