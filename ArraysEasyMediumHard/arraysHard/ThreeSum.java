9e16e22511ca9eafa27a3dbf3542a0f6c62fafff
2023bit602 <parthpatil0512@gmail.com>
2025-05-15 23:58:54 +0530

    3SUM better solution

--- ArraysEasyMediumHard/arraysHard/ThreeSum.java
+++ ArraysEasyMediumHard/arraysHard/ThreeSum.java
@@ -18,4 +18,30 @@ public class ThreeSum{
 		List<List<Integer>> ans = new ArrayList<>(set);
 		return ans;
 	}
+
+	// Better solution using only two loops and hashset
+	List<List<Integer>> threeSumBetter(int[] arr){
+		int n = arr.length;
+		// for storing the triplets
+		Set<List<Integer>> set = new HashSet<>();
+		for(int i = 0; i<n; i++){
+			Set<Integer> hashset = new HashSet<>();
+			for(int j = i+1; j<n; j++){
+				// initialising the third element to find
+				int third = -(arr[i]+arr[j]);
+
+				if(hashset.contains(third)){
+					List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
+					temp.sort(null);
+					set.add(temp);
+				}
+
+				// storing the possible thirds
+				hashset.add(arr[j]);
+			}
+		}
+		List<List<Integer>> ans = new ArrayList<>(set);
+		return ans;
+	}
+
 }
