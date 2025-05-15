891d7b08e474a4f1012596df2cddc4e937d3fbcf
2023bit602 <parthpatil0512@gmail.com>
2025-05-15 16:25:33 +0530

    MAJORITY ELEMENT II optimal approach

--- ArraysEasyMediumHard/arraysHard/MajorityElementII.java
+++ ArraysEasyMediumHard/arraysHard/MajorityElementII.java
@@ -41,4 +41,35 @@ public class MajorityElementII{
 		return ans;
 	}
 
+	// Optimal approach derived using moore's voting algorithm
+	List<Integer> getMajorityElementIIOptimal(int[] arr){
+		int n = arr.length;
+		int el1=Integer.MIN_VALUE, cnt1=0, el2=Integer.MIN_VALUE, cnt2=0;
+		for(int i = 0; i<n; i++){
+			if(cnt1==0 && el2!=arr[i]){
+				cnt1=1;
+				el1=arr[i];
+			} else if(cnt2==0 && el1!=arr[i]){
+				cnt2=1;
+				el2=arr[i];
+			} else if(el1==arr[i]){
+				cnt1++;
+			} else if(el2==arr[i]){
+				cnt2++;
+			} else{
+				cnt1--;
+				cnt2--;
+			}
+		}
+		List<Integer> ans = new ArrayList<>();
+		int el1Cnt=0, el2Cnt=0;
+		for(int i = 0; i<n; i++){
+			if(arr[i] == el1) el1Cnt++;
+			if(arr[i] == el2) el2Cnt++;
+		}
+		if(el1Cnt > n/3) ans.add(el1);
+		if(el2Cnt > n/3) ans.add(el2);
+
+		return ans;
+	}
 }
