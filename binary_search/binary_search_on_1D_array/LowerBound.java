8e4f8c494dc6d715f2efb883308191963d3dee4a
2023bit602 <parthpatil0512@gmail.com>
2025-05-18 22:34:32 +0530

    Lower Bound

---
+++ binary_search/binary_search_on_1D_array/LowerBound.java
@@ -0,0 +1,19 @@
+public class LowerBound {
+	int lowerBound(int[] arr, int x){
+		int n = arr.length;
+		int low = 0;
+		int high = n-1;
+		int ans = n;
+
+		while(low <= high){
+			int mid = (low + high)/2;
+			if(arr[mid] >= x){
+				ans = mid;
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return ans;
+	}
+}
--- binary_search/binary_search_on_1D_array/Main.java
+++ binary_search/binary_search_on_1D_array/Main.java
@@ -2,7 +2,11 @@ public class Main{
 	
 	public static void main(String[] args){
 		BinarySearch obj1 = new BinarySearch();
-		int arr[] = {3,4,6,7,9,12,16,17};
-		System.out.println(obj1.binarySearchRecursive(arr, 0, arr.length - 1, 9));
+		// int arr[] = {3,4,6,7,9,12,16,17};
+		// System.out.println(obj1.binarySearchRecursive(arr, 0, arr.length - 1, 9));
+
+		LowerBound obj2 = new LowerBound();
+		int arr[] = {1,2,3,3,4,8,9,9,9,11};
+		System.out.println(obj2.lowerBound(arr, 3939));
 	}
 }
