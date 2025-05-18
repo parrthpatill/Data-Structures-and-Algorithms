e8ac7d0a09088125be7e0bc2a4662bddbd9d4c0f
2023bit602 <parthpatil0512@gmail.com>
2025-05-18 22:54:59 +0530

    Upper Bound

---
+++ binary_search/binary_search_on_1D_array/LowerBoundAndUpperBound.java
@@ -0,0 +1,40 @@
+public class LowerBoundAndUpperBound {
+
+	// Lower Bound
+	int lowerBound(int[] arr, int x){
+		int n = arr.length;
+		int low = 0;
+		int high = n-1;
+		int ans = n;
+
+		while(low <= high){
+			int mid = (low + high)/2;
+			if(arr[mid] >= x){
+				ans = mid;
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return ans;
+	}
+
+	// Upper Bound
+	int upperBound(int[] arr, int x){
+		int n = arr.length;
+		int low = 0;
+		int high = n-1;
+		int ans = n;
+
+		while(low <= high){
+			int mid = (low + high)/2;
+			if(arr[mid] > x){
+				ans = mid;
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return ans;
+	}
+}
--- binary_search/binary_search_on_1D_array/Main.java
+++ binary_search/binary_search_on_1D_array/Main.java
@@ -5,8 +5,10 @@ public class Main{
 		// int arr[] = {3,4,6,7,9,12,16,17};
 		// System.out.println(obj1.binarySearchRecursive(arr, 0, arr.length - 1, 9));
 
-		LowerBound obj2 = new LowerBound();
+		LowerBoundAndUpperBound obj2 = new LowerBoundAndUpperBound();
 		int arr[] = {1,2,3,3,4,8,9,9,9,11};
 		System.out.println(obj2.lowerBound(arr, 3939));
+		System.out.println(obj2.upperBound(arr, 3));
+
 	}
 }
