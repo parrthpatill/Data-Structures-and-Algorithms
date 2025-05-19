45b6eb627799dd4ef3408280dd1e0077779f83c9
2023bit602 <parthpatil0512@gmail.com>
2025-05-19 12:44:12 +0530

    Floor in an array

--- binary_search/binary_search_on_1D_array/Main.java
+++ binary_search/binary_search_on_1D_array/Main.java
@@ -6,9 +6,13 @@ public class Main{
 		// System.out.println(obj1.binarySearchRecursive(arr, 0, arr.length - 1, 9));
 
 		LowerBoundAndUpperBound obj2 = new LowerBoundAndUpperBound();
-		int arr[] = {1,2,3,3,4,8,9,9,9,11};
-		System.out.println(obj2.lowerBound(arr, 3939));
-		System.out.println(obj2.upperBound(arr, 3));
+		// int arr[] = {1,2,3,3,4,8,9,9,9,11};
+		// System.out.println(obj2.lowerBound(arr, 3939));
+		// System.out.println(obj2.upperBound(arr, 3));
+
+		FloorInArray obj3 = new FloorInArray();
+		int[] arr = {1,2,3,4,4,5,6,8,8,9};
+		System.out.println(obj3.floorInArray(arr, 7));
 
 	}
 }
