7110df20d2b1cf9a57f9e7c824eed2483e8170ae
2023bit602 <parthpatil0512@gmail.com>
2025-05-21 22:14:40 +0530

    Nth root of M brute and optimal

--- binary_search/binary_search_on_answers/Main.java
+++ binary_search/binary_search_on_answers/Main.java
@@ -1,6 +1,9 @@
 public class Main{
 	public static void main(String[] args){
-		Sqrt obj11 = new Sqrt();
-		System.out.println(obj11.sqrtOfIntegerOptimal(35));
+		// Sqrt obj1 = new Sqrt();
+		// System.out.println(obj1.sqrtOfIntegerOptimal(35));
+
+		NthRootOfM obj2 = new NthRootOfM();
+		System.out.println(obj2.nthRootOfMOptimal(4,16));
 	}
 }
---
+++ binary_search/binary_search_on_answers/NthRootOfM.java
@@ -0,0 +1,32 @@
+public class NthRootOfM{
+	int func(int i, int n){
+		int prod = 1;
+		for(int j = 1; j<=n; j++){
+			prod = prod * i;
+		}
+		return prod;
+	}
+	// brute force solution
+	int nthRootOfMBrute(int n, int m){
+		for(int i=1; i<=m; i++){
+			int prod = func(i, n);
+			if(prod == m) return i;
+			else if(prod > m) break;
+		}
+		return -1;
+	}
+
+	//optimal solution
+	int nthRootOfMOptimal(int n, int m){
+		int low = 1;
+		int high = m;
+		while(low <= high){
+			int mid = (low+high)/2;
+			int prod = func(mid, n);
+			if(prod == m) return mid;
+			else if(prod < m) low = mid + 1;
+			else high = mid - 1;
+		}
+		return -1;
+	}
+}
