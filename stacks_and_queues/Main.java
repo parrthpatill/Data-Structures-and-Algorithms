bdb51ff5dd82c215392fa341713a23cd288306c9
2023bit602 <parthpatil0512@gmail.com>
2025-06-10 04:45:23 +0530

    Queue implementation using arrays

--- stacks_and_queues/Main.java
+++ stacks_and_queues/Main.java
@@ -1,15 +1,7 @@
 public class Main{
 	public static void main(String[] args){
 		StackUsingArray obj1 = new StackUsingArray(3);
-		obj1.push(3);
-		obj1.push(7);
-		obj1.pop();
-		obj1.pop();
-
-		System.out.println(obj1.top());
-
-
-
+		QueueUsingArray obj2 = new QueueUsingArray(3);
 		
 
 	}
---
+++ stacks_and_queues/QueueUsingArray.java
@@ -0,0 +1,55 @@
+public class QueueUsingArray{
+	int size;
+	int currSize = 0;
+	int start = -1;
+	int end = -1;
+	int[] queue;
+
+	QueueUsingArray(int size){
+		this.size = size;
+		queue = new int[size];
+	}
+
+	void push(int x){
+		if(size == currSize){
+			System.out.println("Queue full");
+			return;	
+		} 
+		if(currSize == 0){
+			start += 1;
+			end += 1;
+		} else {
+			end = (end+1) % size;
+		}
+		queue[end] = x;
+		currSize += 1;
+	}
+
+	int pop(){
+		if(currSize == 0){
+			System.out.println("Stack empty");
+			return 220905;
+		}
+		int element = queue[start];
+		if(currSize == 1){
+			start = -1;
+			end = -1;
+		} else {
+			start = (start + 1) % size;
+		}
+		currSize -= 1;
+		return element;
+	}
+
+	int front(){
+		if(currSize == 0) {
+			System.out.println("Stack Empty");
+			return 220905;
+		}
+		return queue[start];
+	}
+
+	int size(){
+		return currSize;
+	}
+}
