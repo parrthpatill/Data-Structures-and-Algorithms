80af0a04508ee8c6de900dd820498a5970188062
2023bit602 <parthpatil0512@gmail.com>
2025-06-16 01:07:30 +0530

    Implement Min in Stack

--- stacks_and_queues/conversions/PostfixToPrefix.java
+++ stacks_and_queues/conversions/PostfixToPrefix.java
@@ -1,3 +1,16 @@
+import java.util.Stack;
 public class PostfixToPrefix {
-	
+	String postfixToPrefix(String s){
+		Stack<String> st = new Stack<>();
+		for(char ch : s.toCharArray()){
+			if(Character.isLetterOrDigit(ch)){
+				st.push(Character.toString(ch));
+			} else {
+				String t1 = st.pop();
+				String t2 = st.pop();
+				st.push(ch + t2 + t1);
+			}
+		}
+		return st.peek();
+	}
 }
